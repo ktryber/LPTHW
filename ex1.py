31401146d714dda2933d7081a58818521a10acd1
Kris <Ktryber@gmail.com>
2015-03-04 07:12:33 -0500

    First Commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "i'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "I wish i knew terminal was the Python interpreter"
+#this is to comment stuff
---
+++ ex3.py
@@ -0,0 +1,15 @@
+# + plus == addition
+# - minus == subtraction
+# / slash == division
+# * asterik == multiplication
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+# == equal-to
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 %4
