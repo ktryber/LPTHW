5b0f826107545a2ad40e203fcdd8dc3d6fef9b0f
Kris <Ktryber@gmail.com>
2015-03-06 04:35:56 -0500

    ex7

---
+++ ex7.py
@@ -0,0 +1,27 @@
+# print a string
+print "Mary had a little lamb."
+# the %s place holds for the string to the right 'snow'
+print "Its fleece was white as %s." % 'snow'
+print "And everwhere that Mary went."
+print "." * 10 # what'd that do? # i think this will print a period 10 times, i was correct
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11= "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+#removing the comma makes cheeseburger print on two lines instead of one.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9+ end10 + end11 + end12
+
+# having python go longer than 80 characters on one line is bad style
+# single quotes are typically used for shorter strings like 'a' and 'snow'.
