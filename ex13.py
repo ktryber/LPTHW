7d49751f7cd27f6cdf4abee6358a0eddfe12932a
Kris <Ktryber@gmail.com>
2015-03-11 07:38:01 -0400

    3/9/15

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print raw_input("Your first variable is:"), first
+print raw_input("Your second variable is:"), second
+print raw_input("Your third variable is:"), third
+print raw_input("Let's combine raw input")
